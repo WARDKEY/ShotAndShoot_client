caab8343016fddc7c910e5a658b742c29f76caba
WARDKEY <tjrmsduq@naver.com>
2024-12-24 13:35:09 +0900

    feat(login_screen) : 로그인 페이지 구현

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -20,12 +21,141 @@ class _LoginScreenState extends State<LoginScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-              Provider.of<AppState>(context, listen: false).onItemTapped(0);
-            },
-            icon: Icon(Icons.home)),
+              onPressed: () {
+                Navigator.pop(context);
+                Provider.of<AppState>(context, listen: false).onItemTapped(0);
+              },
+              icon: Icon(Icons.home)),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("카카오 로그인");
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow,
+                minimumSize: Size.fromHeight(80),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "images/kakao.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: 55,
+                  ),
+                  Text(
+                    "카카오 로그인",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print("네이버 로그인");
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: Size.fromHeight(80),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.asset(
+                    "images/naver.png",
+                    width: 40,
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: 55,
+                  ),
+                  Text(
+                    "네이버 로그인",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 45,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    border: Border(
+                        top: BorderSide(
+                  color: Colors.grey,
+                  width: 1.0,
+                ))),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: '계정이 없으신가요?  ',
+                            style: const TextStyle(
+                              fontSize: 15,
+                              color: Colors.black,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: '회원가입',
+                                style: const TextStyle(
+                                  color: Colors.blueAccent,
+                                  fontSize: 15,
+                                ),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    // 눌렀을 때 갈 곳 설정 = 회원가입 페이지
+                                    print("회원가입");
+                                  },
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
